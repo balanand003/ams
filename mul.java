74e3e41e3d9bf54c60161dbe34bb1bd94779e7c9
balanand003 <balanandm01@gmail.com>
2018-09-24 10:05:05 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Mul
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter a number:");
+int n=s.nextInt();
+for(int i=1;i<=10;i++)
+{
+System.out.println(n+" * "+i+" = "+n*i);
+}
+}
+}
